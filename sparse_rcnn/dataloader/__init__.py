211de7116b510d6a073ac657a99fbb75c0b4ea6c
Lazurite <lazurite@tom.com>
2022-05-26 20:29:00 +0800

    突然知道为什么使用这个sampler了

--- sparse_rcnn/dataloader/__init__.py
+++ sparse_rcnn/dataloader/__init__.py
@@ -1,8 +1,8 @@
 from torch.utils.data import DataLoader
-from torch.utils.data.distributed import DistributedSampler
-from .sampler import RandomSampler, AspectRatioBasedSampler, DistributedGroupSampler
-from .dataset import CocoDataset
+
 from .collate import Collate
+from .dataset import CocoDataset
+from .sampler import RandomSampler, AspectRatioBasedSampler, DistributedGroupSampler
 
 __all__ = {
     "CocoDataset": CocoDataset
@@ -11,30 +11,42 @@ __all__ = {
 
 def build_dataloader(dataset_cfg, transforms, batch_size, dist, workers=4, pin_memory=True,
                      mode="train"):
-    if dist and mode == "val":
-        raise ValueError("DDP currently does not support validation")
-
     if dataset_cfg.DATASET not in __all__.keys():
         raise ValueError("Dataset {} not supported".format(dataset_cfg.DATASET))
 
     dataset = __all__[dataset_cfg.DATASET](dataset_cfg, mode, transforms)
 
-    if dist:
-        # sampler = DistributedSampler(dataset, shuffle=(mode == "train"))
-        sampler = DistributedGroupSampler(dataset, samples_per_gpu=batch_size)
+    if mode == "train":
+        if dist:
+            # sampler = DistributedSampler(dataset, shuffle=(mode == "train"))
+            sampler = DistributedGroupSampler(dataset, samples_per_gpu=batch_size)
+        else:
+            # Note: Use this sampler to reduce memory usage.
+            sampler = AspectRatioBasedSampler(dataset, batch_size=batch_size, drop_last=True)
+
+        dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,  # Not used
+            pin_memory=pin_memory,
+            num_workers=workers,
+            shuffle=True,  # Not used
+            collate_fn=Collate(dataset_cfg),
+            drop_last=False,
+            sampler=sampler,
+        )
+    elif mode == "val":
+        if dist:
+            raise ValueError("DDP currently does not support validation")
+        else:
+            dataloader = DataLoader(
+                dataset,
+                batch_size=batch_size,
+                pin_memory=pin_memory,
+                num_workers=workers,
+                shuffle=False,
+                collate_fn=Collate(dataset_cfg),
+                drop_last=False
+            )
     else:
-        # Note: Use this sampler to reduce memory usage.
-        sampler = AspectRatioBasedSampler(dataset, batch_size=batch_size, drop_last=True)
-
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        pin_memory=pin_memory,
-        num_workers=workers,
-        shuffle=(mode == "train"),
-        collate_fn=Collate(dataset_cfg),
-        drop_last=False,
-        sampler=sampler if mode == "train" else None,
-    )
-
+        raise ValueError("Mode {} not supported".format(mode))
     return dataloader
