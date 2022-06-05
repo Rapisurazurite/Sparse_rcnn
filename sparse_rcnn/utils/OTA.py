5683678ffdf859e6c92fbbcd30e324965d2c64b2
Lazurite <lazurite@tom.com>
2022-06-05 16:24:36 +0800

    test

--- sparse_rcnn/utils/OTA.py
+++ sparse_rcnn/utils/OTA.py
@@ -160,7 +160,8 @@ class OtaMatcher(nn.Module):
             pi = pi / rescale_factor.unsqueeze(1)
             max_assigned_units, matched_gt_inds = torch.max(pi, dim=0)
 
-            prop_indice = (matched_gt_inds != num_gt).nonzero().squeeze()
+            # dude ! 
+            prop_indice = (matched_gt_inds != num_gt).nonzero().squeeze(-1)
             gt_indice = matched_gt_inds[prop_indice]
             indice.append((prop_indice, gt_indice))
         return indice
